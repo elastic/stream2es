d8c677384b42bce7dc7ac44fa5dc6bece13b7b3f
Andrew Raines <drew@raines.me>
2013-03-11 12:50:09 -0500

    clojure-1.5.1.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :resource-paths ["etc" "resources"]
   :dependencies [[cheshire "5.0.1"]
                  [clj-http "0.6.3"]
-                 [org.clojure/clojure "1.5.0-RC16"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.elasticsearch/elasticsearch-river-wikipedia
